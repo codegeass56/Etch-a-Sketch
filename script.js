954cd43068994c049782c9651fc13fae43b8060c
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2022-08-15 16:44:13 +0400

    Change grid to inline and grid-items to square size
    Changed the grid to an inline grid to ignore space at the end of each
    column. Also modified the size of each grid-item to 40px by 40px.
    Ultimately, created a square grid sized 640px by 640 px.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const mainContainer = document.createElement('div');
 mainContainer.classList.add('main-container');
 document.querySelector('body').appendChild(mainContainer);
-mainContainer.style.display = 'grid';
-mainContainer.style.gridTemplateColumns = 'repeat(16,auto)';
+mainContainer.style.display = 'inline-grid';
+mainContainer.style.gridTemplateColumns = 'repeat(16,40px)';
+mainContainer.style.gridTemplateRows = 'repeat(16,40px)';
+
 
 for (let i = 0; i < 256; i++) {
   const gridItem = document.createElement('div');
