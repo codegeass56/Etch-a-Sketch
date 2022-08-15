f1fc796f1f0d578a62892c11154e9335bcde1db4
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2022-08-15 18:01:49 +0400

    Remove grid numbering and add custom grid size
    Removed the grid numbering and added the logic to customize the grid
    size. Given a size, a square grid will be created within the
    fixed space of the main container div (Set to 640 x 640).

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ mainContainer.style.height = '640px';
 
 //Create a grid template (default grid size is 16 x 16)
 mainContainer.style.display = 'inline-grid';
-
 let gridSize = 16;
-mainContainer.style.gridTemplateColumns = `repeat(${gridSize},40px)`;
-mainContainer.style.gridTemplateRows = `repeat(${gridSize},40px)`;
+
+//Add grid items with equal width and height as per grid size
+mainContainer.style.gridTemplateColumns = `repeat(${gridSize},auto)`;
+mainContainer.style.gridTemplateRows = `repeat(${gridSize},auto)`;
 
 //Add 16 x 16 grid items to the grid
 for (let i = 0; i < Math.pow(gridSize, 2); i++) {
   const gridItem = document.createElement('div');
   gridItem.classList.add('grid-item');
-  gridItem.textContent = `${i + 1}`;
   mainContainer.appendChild(gridItem);
   gridItem.addEventListener('mouseenter', () => {
     gridItem.style.backgroundColor = 'black';
