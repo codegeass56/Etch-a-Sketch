4a679502652f875394c41ace99b09a229c9a6d15
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2022-08-15 17:51:36 +0400

    Abstract the grid dimensions and add some comments

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+//Create the main container
 const mainContainer = document.createElement('div');
 mainContainer.classList.add('main-container');
 document.querySelector('body').appendChild(mainContainer);
+
+//Set its default size
+mainContainer.style.width = '640px';
+mainContainer.style.height = '640px';
+
+//Create a grid template (default grid size is 16 x 16)
 mainContainer.style.display = 'inline-grid';
-mainContainer.style.gridTemplateColumns = 'repeat(16,40px)';
-mainContainer.style.gridTemplateRows = 'repeat(16,40px)';
 
+let gridSize = 16;
+mainContainer.style.gridTemplateColumns = `repeat(${gridSize},40px)`;
+mainContainer.style.gridTemplateRows = `repeat(${gridSize},40px)`;
 
-for (let i = 0; i < 256; i++) {
+//Add 16 x 16 grid items to the grid
+for (let i = 0; i < Math.pow(gridSize, 2); i++) {
   const gridItem = document.createElement('div');
   gridItem.classList.add('grid-item');
   gridItem.textContent = `${i + 1}`;
