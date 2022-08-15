e42d922c2373d71f9562eb0376cf4988327172be
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2022-08-15 16:36:26 +0400

    Create 16 by 16 grid containing auto-size grid items

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const mainContainer = document.createElement('div');
 mainContainer.classList.add('main-container');
 document.querySelector('body').appendChild(mainContainer);
+mainContainer.style.display = 'grid';
+mainContainer.style.gridTemplateColumns = 'repeat(16,auto)';
+
+for (let i = 0; i < 256; i++) {
+  const gridItem = document.createElement('div');
+  gridItem.classList.add('grid-item');
+  gridItem.textContent = `${i + 1}`;
+  mainContainer.appendChild(gridItem);
+}
